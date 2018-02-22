445341ba4e6b6cc25991b055b12b5ddb63192339
Admin <aumpor59@snru.ac.th>
2018-02-22 11:59:47 +0700

    before Lunch

---
+++ app/src/main/java/masterkengamporn/snru/ac/th/emercall/utility/HopitalAdapter.java
@@ -0,0 +1,66 @@
+package masterkengamporn.snru.ac.th.emercall.utility;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import masterkengamporn.snru.ac.th.emercall.R;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
+
+/**
+ * Created by Admin on 22/2/2561.
+ */
+
+public class HopitalAdapter extends BaseAdapter{
+
+    //    Explicit
+    private Context context;
+    private int[] ints;
+    private String[] titleStrings, phoneStrings;
+
+    public HopitalAdapter(Context context, int[] ints, String[] titleStrings, String[] phoneStrings) {
+        this.context = context;
+        this.ints = ints;
+        this.titleStrings = titleStrings;
+        this.phoneStrings = phoneStrings;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(LAYOUT_INFLATER_SERVICE);
+        View view1 = layoutInflater.inflate(R.layout.listview_layout, viewGroup, false);
+
+
+        ImageView imageView = view1.findViewById(R.id.imageListView);
+        TextView titleTextVioew = view1.findViewById(R.id.imageListView);
+        TextView phoneTextView = view1.findViewById(R.id.textViewPhone);
+
+        imageView.setImageResource(ints[i]);
+        titleTextVioew.setText(titleStrings[i]);
+        phoneTextView.setText(phoneStrings[i]);
+
+
+        return view1;
+    }
+}   // Main Class
